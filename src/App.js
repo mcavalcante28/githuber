84decce40b1eaaa47fe944a0ab3b305e1a6cf93e
Marcus Cavalcante <mgc.cavalcante28@gmail.com>
2020-04-06 18:07:21 -0300

    Alterações na estilização da página

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo3 from './assets/logo3.png'
-import logo from './assets/logo.png'
+import logo2 from './assets/logo2.png'
 import { GoRepoForked, GoStar } from "react-icons/go";
 import './styles.css'
 
@@ -17,26 +17,31 @@ function App() {
         <input placeholder="Username" className="input-class"/>
         <button type="submit" className="button"> Confirmar </button>
         </form>
-        <div className="userInfo-container">
-          <p>Bem vindo, USER</p>
-          <img src={logo} alt="img"></img>
-          <p> Followers: 30</p>
-          <p> Repositories: 30 </p>
         </div>
-        </div>
-      <div className="right-container"> 
+      <div className="right-container">
+      <div className="userInfo-container">
+          <p className="user-p">Bem vindo, USER</p>
+          <div>
+            <img src={logo2} alt="img"></img> 
+            <div>
+              <p> Followers: 30</p>
+              <p> Repositories: 30 </p>
+            </div>
+          </div>
+          <p className="user-p">Top 4 Repositórios:</p>
+        </div> 
       <section className="section-items">
         {repos.map(item =>(
           <div>
           <p> NOME </p>
+          <div>
           <p> 
-          <GoRepoForked size={16} color="#F2F2F2" />
-           Forks: 30
+          <GoRepoForked size={16} color="#F2F2F2" />30
           </p>
           <p> 
-          <GoStar size={16} color="#F2F2F2" />
-           Stars: 20
+          <GoStar size={16} color="#F2F2F2" />20
           </p>
+          </div>
         </div>
         ))}
       </section>
